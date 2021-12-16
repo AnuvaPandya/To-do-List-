06e81823f78f24281c86ffe89780db1c60fb0362
AnuvaPandya <87873813+AnuvaPandya@users.noreply.github.com>
2021-12-16 10:12:28 +0530

    styled components

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -28,12 +28,12 @@ const Button = styled.button`
   }
 `;
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
-
 export default Button;
+
+const Button = (props) => {
+  return (
+    <button type={props.type} className="button" onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
